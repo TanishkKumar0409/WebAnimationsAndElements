579dcd77797dd728bbdfb31d2f73f0bcb260438d
TanishkKumar0409 <tanishkk60@gmail.com>
2024-07-04 21:29:47 +0530

    added offcanvas navigation bar

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/* offcanvas navigation bar */
+function openNav() {
+    document.getElementById("myNav").style.right = "0";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.right = "-250px";
+  }
+  /* offcanvas navigation bar */
