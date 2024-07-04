59be4b60bea46083fc5aae86ca7fa620239f58e0
TanishkKumar0409 <tanishkk60@gmail.com>
2024-07-04 21:36:22 +0530

    Added ripple parallax effect carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,69 @@ function openNav() {
     document.getElementById("myNav").style.right = "-250px";
   }
   /* offcanvas navigation bar */
+  
+/* home page ripple carousel */
+const carousel = document.querySelector(".r-carousel-div");
+const items = Array.from(document.querySelectorAll(".r-carousel-item"));
+let autoPlayInterval;
+const autoPlayDelay = 5000;
+
+function moveToNextSlide() {
+  const currentSlide = items.shift();
+  items.push(currentSlide);
+  updateSlides();
+}
+
+function moveToPrevSlide() {
+  const lastSlide = items.pop();
+  items.unshift(lastSlide);
+  updateSlides();
+}
+
+function updateSlides() {
+  items.forEach((item, index) => {
+    item.style.zIndex = items.length - index;
+    item.style.transform = "translateX(0)";
+  });
+  items[items.length - 1].style.transform = "translateX(100%)";
+  setTimeout(() => {
+    items[items.length - 1].style.transition = "none";
+    items[items.length - 1].style.transform = "translateX(0)";
+    setTimeout(() => {
+      items[items.length - 1].style.transition =
+        "transform 0.5s ease-in-out";
+    }, 50);
+  }, 500);
+}
+
+function nextSlide() {
+  clearInterval(autoPlayInterval);
+  items[0].style.transform = "translateX(-100%)";
+  setTimeout(() => {
+    moveToNextSlide();
+    autoPlay();
+  }, 500);
+}
+
+function prevSlide() {
+  clearInterval(autoPlayInterval);
+  items[items.length - 1].style.transition = "none";
+  items[items.length - 1].style.transform = "translateX(-100%)";
+  setTimeout(() => {
+    moveToPrevSlide();
+    items[0].style.transition = "transform 0.5s ease-in-out";
+    items[0].style.transform = "translateX(0)";
+    autoPlay();
+  }, 50);
+}
+
+function autoPlay() {
+  clearInterval(autoPlayInterval);
+  autoPlayInterval = setInterval(nextSlide, autoPlayDelay);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateSlides();
+  autoPlay();
+});
+/* home page ripple carousel */
