94d48683ffe7a1e66e40b6794385259655ff8b44
TanishkKumar0409 <tanishkk60@gmail.com>
2024-07-06 17:19:09 +0530

    remove html and added php for the project

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+  <script src="https://kit.fontawesome.com/87f0afa689.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+  <script src="assets/js/jquery-3.7.1.min.js"></script>
+  <style>
+ 
+  </style>
+</head>
+<body>
+  <!-- Off-Canvas Navigation Bar -->
+  <section class="navigation">
+    <div class="logo-div">
+      <img src="assets/images/logo/png-logo.png" class="logo" alt="Logo" />
+    </div>
+    <div id="myNav" class="off-nav">
+      <div class="c-btn d-flex p-2">
+        <img src="assets/images/logo/png-logo.png" width="80%" alt="" />
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      </div>
+      <a href="index.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="#">Services</a>
+      <a href="#">Results</a>
+      <a href="#">Blogs</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
+      <a href="#">Careers with us</a>
+      <a href="#">Gallery</a>
+    </div>
+    <button class="openbtn p-2" onclick="openNav()">&#9776; Menu</button>
+  </section>
+  <!-- Off-Canvas Navigation Bar -->
---
+++ index.php
@@ -0,0 +1,378 @@
+<?php
+include "header.php";
+?>
+<!-- Ripple Carousel -->
+<section class="rc-section">
+  <div class="r-carousel">
+    <div class="r-carousel-div">
+      <div class="r-carousel-item ripple" style="z-index: 4">
+        <h3 class="text-light px-5">
+          Branding & Marketing Pitstop For Your Business Growth
+        </h3>
+      </div>
+      <div class="r-carousel-item ripple" style="z-index: 3">
+        <h3 class="text-light px-5">
+          Branding & Marketing Pitstop For Your Business Growth
+        </h3>
+      </div>
+      <div class="r-carousel-item ripple" style="z-index: 2">
+        <h3 class="text-light px-5">
+          Branding & Marketing Pitstop For Your Business Growth
+        </h3>
+      </div>
+      <div class="r-carousel-item ripple" style="z-index: 1">
+        <h3 class="text-light px-5">
+          Branding & Marketing Pitstop For Your Business Growth
+        </h3>
+      </div>
+    </div>
+    <button class="nav-button prev" onclick="prevSlide()">&#10094;</button>
+    <button class="nav-button next" onclick="nextSlide()">&#10095;</button>
+  </div>
+</section>
+<!-- Ripple Carousel -->
+<!-- Moving Numbers -->
+<section class="moving-numbers mb-3 text-center container">
+  <div class="glass">
+    <div class="countbox d-flex justify-center items-center">
+      <h2 class="fs-2">
+        <h3 class="counter" data-max="50"></h3>
+        <i class="fa-solid fa-plus"></i>
+      </h2>
+    </div>
+    <p>Clients</p>
+  </div>
+  <div class="glass">
+    <div class="countbox d-flex justify-center items-center">
+      <h2>
+        <h3 class="counter" data-max="29"></h3>
+        <i class="fa-solid fa-plus"></i>
+      </h2>
+    </div>
+    <p>Project</p>
+  </div>
+
+  <div class="glass">
+    <div class="countbox d-flex justify-center items-center">
+      <h2>
+        <h3 class="counter" data-max="80"></h3>
+        <i class="fa-solid fa-plus"></i>
+      </h2>
+    </div>
+    <p>Countries</p>
+  </div>
+
+  <div class="glass">
+    <div class="countbox d-flex justify-center items-center">
+      <h2>
+        <h3 class="counter" data-max="60"></h3>
+        <i class="fa-solid fa-plus"></i>
+      </h2>
+    </div>
+    <p>Satisfactions</p>
+  </div>
+</section>
+<!-- Moving Numbers -->
+<!-- About Section -->
+<section class="about-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 text-start p-lg-5">
+        <h3>Who We Are?</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+          deleniti corporis minus tenetur itaque expedita consectetur
+          repudiandae illo quae libero, voluptates aliquid rem, ut veritatis
+          quasi tempora mollitia fugiat? Ullam odit illo voluptatum unde.
+          Veniam autem et vel quisquam debitis quod adipisci facere voluptas
+          repellendus qui laudantium, eum quasi, accusantium enim velit
+          exercitationem alias magnam dignissimos repellat iure dolorem
+          deserunt expedita ipsum. Magnam reprehenderit iste provident,
+          veritatis dicta, sunt soluta facere temporibus a ea tempora
+          consequuntur iure numquam blanditiis laborum quisquam facilis quos
+          at rerum dignissimos modi vel error? Similique quam voluptas earum
+          amet eum nulla dolore dignissimos eaque cupiditate.
+        </p>
+        <br />
+        <button class="btn btn-info">Learn more</button>
+      </div>
+      <div class="col-lg-6 text-start">
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/011/019/585/small_2x/social-media-and-digital-marketing-3d-illustration-png.png" width="80%" alt="" />
+      </div>
+    </div>
+  </div>
+</section>
+<!-- About Section -->
+<!-- Services Section -->
+<section class="services container-fluid bg-secondary-subtle">
+  <div class="row p-md-5 py-5">
+    <div class="col text-center px-md-5">
+      <h2>Services</h2>
+      <p class="px-md-5 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At ducimus
+        ex libero ab, quas et. Rerum quasi tempore minima error possimus
+        nulla fugiat magni? Dolores voluptatibus magnam numquam excepturi
+        suscipit!
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <ul id="categories" class="clr">
+        <li>
+          <div>
+            <img src="https://farm3.staticflickr.com/2878/10944255073_973d2cd25c.jpg" alt="" />
+            <div class="heading">
+              <h1>Performance Marketing</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm9.staticflickr.com/8461/8048823381_0fbc2d8efb.jpg" alt="" />
+            <div class="heading">
+              <h1>Brading and Strategy</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm5.staticflickr.com/4144/5053682635_b348b24698.jpg" alt="" />
+            <div class="heading">
+              <h1>Social Media Marketing</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm3.staticflickr.com/2827/10384422264_d9c7299146.jpg" alt="" />
+            <div class="heading">
+              <h1>Search Engine Optimization</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm7.staticflickr.com/6217/6216951796_e50778255c.jpg" alt="" />
+            <div class="heading">
+              <h1>Influencer Marketing</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm7.staticflickr.com/6083/6055581292_d94c2d90e3.jpg" alt="" />
+            <div class="heading">
+              <h1>Video Marketing</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm7.staticflickr.com/6092/6227418584_d5883b0948.jpg" alt="" />
+            <div class="heading">
+              <h1>Online Reputation Management</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm8.staticflickr.com/7187/6895047173_d4b1a0d798.jpg" alt="" />
+            <div class="heading">
+              <h1>Email Marketing</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div>
+            <img src="https://farm4.staticflickr.com/3766/12953056854_b8cdf14f21.jpg" alt="" />
+            <div class="heading">
+              <h1>Content Marketing</h1>
+            </div>
+            <div class="text">
+              <p>
+                Some sample text about the article this hexagon leads to
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <a href="#" class="float-end me-5 text-dark text-decoration-none link-hover">Learn More <i class="fa-solid fa-arrow-right"></i></a>
+    </div>
+  </div>
+</section>
+<!-- Services Section -->
+<!-- Parallax Image -->
+<section class="parallax"></section>
+<!-- Parallax Image -->
+<!-- Parallax Image -->
+<section class="parallax"></section>
+<!-- Parallax Image -->
+<!-- Testimonial -->
+<section class="testimonial bg-secondary-subtle container-fluid text-center p-md-5">
+  <div class="row p-md-5 py-5">
+    <div class="col">
+      <div class="testimonial-head">
+        <h2>Testimonial</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+          assumenda culpa tempora saepe perspiciatis consequatur labore
+          inventore quam in dolorem eius enim molestias tenetur, quia sed
+          facere excepturi. Sit, placeat.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active px-md-5">
+        <div class="img d-flex justify-content-center">
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/006/487/917/small_2x/man-avatar-icon-free-vector.jpg" class="d-block test-image" alt="..." />
+        </div>
+        <div class="img-text">
+          <p>
+            <i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i>
+          </p>
+          <h3>Heera Thakur</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            cumque distinctio repudiandae odio eveniet, ex minima quae natus
+            obcaecati esse corrupti sequi eos cum accusamus, possimus
+            doloribus delectus dolor? Corporis?
+          </p>
+        </div>
+      </div>
+      <div class="carousel-item px-md-5">
+        <div class="img d-flex justify-content-center">
+          <img src="https://static.vecteezy.com/system/resources/previews/002/002/297/non_2x/beautiful-woman-avatar-character-icon-free-vector.jpg" class="d-block test-image" alt="..." />
+        </div>
+        <div class="img-text">
+          <p>
+            <i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i>
+          </p>
+          <h3>Shruti Srivastava</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            cumque distinctio repudiandae odio eveniet, ex minima quae natus
+            obcaecati esse corrupti sequi eos cum accusamus, possimus
+            doloribus delectus dolor? Corporis?
+          </p>
+        </div>
+      </div>
+      <div class="carousel-item px-md-5">
+        <div class="img d-flex justify-content-center">
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/006/487/917/small_2x/man-avatar-icon-free-vector.jpg" class="d-block test-image" alt="..." />
+        </div>
+        <div class="img-text">
+          <p>
+            <i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i><i class="fa-solid fa-star text-warning"></i>
+          </p>
+          <h3>Pratyaksh Tiwari</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            cumque distinctio repudiandae odio eveniet, ex minima quae natus
+            obcaecati esse corrupti sequi eos cum accusamus, possimus
+            doloribus delectus dolor? Corporis?
+          </p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev text-dark fs-1" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      &#10094;
+    </button>
+    <button class="carousel-control-next text-dark fs-1" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      &#10095;
+    </button>
+  </div>
+</section>
+<!-- Testimonial -->
+<!-- Contact -->
+<section class="contact container-fluid p-md-5">
+  <div class="row p-md-5 py-5 text-center">
+    <div class="col">
+      <div class="Contact-head">
+        <h2>Contact</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+          assumenda culpa tempora saepe perspiciatis consequatur labore
+          inventore quam in dolorem eius enim molestias tenetur, quia sed
+          facere excepturi. Sit, placeat.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center contact-form">
+    <div class="col p-3">
+      <form>
+        <div class="mb-3">
+          <input type="text" class="form-control" placeholder="Name" />
+        </div>
+        <div class="mb-3">
+          <input type="email" class="form-control" placeholder="Email" />
+        </div>
+        <div class="mb-3">
+          <input type="number" class="form-control" placeholder="Contact" />
+        </div>
+        <div class="form-floating">
+          <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+          <label for="floatingTextarea">Comments</label>
+        </div>
+        <br />
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</section>
+<iframe style="height: 500px; width: 100%; border: 0; margin: 0" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=offbeat+pixels&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+<!-- Contact -->
+<?php
+include "footer.php";
+?>
