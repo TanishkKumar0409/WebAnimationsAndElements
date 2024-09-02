2b94ed47bcfb6f68d5c5924de116f92af3b18452
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-02 18:32:06 +0530

    update expression.java

--- Expression.java
+++ Expression.java
@@ -3,35 +3,42 @@ import java.util.Random;
 
 public class Expression {
     public static void main(String[] args) {
+        // ! First Expression
         int y = 7;
         int x = ++y * 8;
         System.out.println("the value of x:" + x);
 
+        // ? Second Expression
         float a = 7 / 4f * 9 / 2f;
         System.out.println("the value of a:" + a);
 
+        // * */ Third Expression
         char Grade = 'A';
         Grade += 8;
         System.out.println("The encrypted Grade:" + Grade);
         Grade -= 8;
         System.out.println("The Decrypted Grade:" + Grade);
 
-        Scanner Value=new Scanner(System.in);
-        Random rand=new Random();
+        Scanner Value = new Scanner(System.in);
+        Random rand = new Random();
+
         System.out.print("Please Enter a Number:");
-        int Entered=Value.nextInt();
-        int Genrated=rand.nextInt(1000);
-        System.out.println("The Given Number:"+Genrated);
-        System.out.println("The Entered Number:"+Entered);
-        if(Entered>Genrated){
+        int Entered = Value.nextInt();
+
+        int Genrated = rand.nextInt(1000);
+
+        System.out.println("The Given Number:" + Genrated);
+        System.out.println("The Entered Number:" + Entered);
+
+        if (Entered > Genrated) {
             System.out.println("Entered Number is Greater");
-        }else if(Entered==Genrated){
+        } else if (Entered == Genrated) {
             System.out.println("Both Numbers are Equal");
-        }else if(Entered<Genrated){
+        } else if (Entered < Genrated) {
             System.out.println("Genrated Number is Greater");
         }
-        Value.close();
 
+        Value.close();
 
     }
 }
