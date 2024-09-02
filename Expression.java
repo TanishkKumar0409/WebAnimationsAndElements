6203f86488e0f3f8c39e8fe36900dfe821ba1126
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-02 18:22:47 +0530

    update expression.java

--- Expression.java
+++ Expression.java
@@ -6,5 +6,11 @@ public class Expression {
 
         float a = 7 / 4f * 9 / 2f;
         System.out.println("the value of a:" + a);
+
+        char Grade = 'A';
+        Grade+=8;
+        System.out.println("The encrypted Grade:" + Grade);
+        Grade-=8;
+        System.out.println("The Decrypted Grade:"+Grade);
     }
 }
