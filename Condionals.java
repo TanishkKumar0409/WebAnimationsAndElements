b275d93716e9af899cf10fcf8481b84fe85b81f5
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-04 19:02:42 +0530

    update conditionals.java

--- Condionals.java
+++ Condionals.java
@@ -13,7 +13,22 @@ public class Condionals {
             System.out.println("I am not 11");
         }
 
-      
+        // ? Second Set
+        System.out.print("Enter Marks of Your First Subject:");
+        int First = Value.nextInt();
+        System.out.print("Enter Marks of Your Second Subject:");
+        int Second = Value.nextInt();
+        System.out.print("Enter Marks of Your Third Subject:");
+        int Third = Value.nextInt();
+        int Total = First + Second + Third;
+        Float Percentage = (Total / 300f) * 100;
+        System.out.println(Percentage);
+        if (Percentage >= 40) {
+            System.out.println("Your are Pass");
+        } else if (Percentage <= 33) {
+            System.out.println("Your are Fail");
+        }
+
         Value.close();
     }
 }
