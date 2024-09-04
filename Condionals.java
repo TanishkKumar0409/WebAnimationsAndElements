1a14db54a5806bbdaa990893bc9d6d990cb7abf8
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-04 19:03:19 +0530

    update conditionals.java

--- Condionals.java
+++ Condionals.java
@@ -45,6 +45,33 @@ public class Condionals {
             System.out.println("You Don't Have to Pay Tax");
         }
 
+        // ! Fourth Set
+        System.out.print("Please Enter the Number to Find Day:");
+        int DayNumber = Value.nextInt();
+        switch (DayNumber) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuseday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
         Value.close();
     }
 }
