aea9f0fd0dae3f68e69178b105799ec9755d5149
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-04 19:02:06 +0530

    Create Conditionals.java

---
+++ Condionals.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Condionals {
+
+    public static void main(String[] args) {
+        Scanner Value = new Scanner(System.in);
+        // ! First Set
+        int a = 10;
+        if (a == 11) {
+            System.out.println("I am 11");
+        } else {
+            System.out.println("I am not 11");
+        }
+
+      
+        Value.close();
+    }
+}
