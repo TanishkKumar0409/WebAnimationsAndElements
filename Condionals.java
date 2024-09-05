7eff14e03a1d3c7ffba3014eea449110c5ad0873
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-05 10:13:58 +0530

    update conditionals.java

--- Condionals.java
+++ Condionals.java
@@ -49,27 +49,20 @@ public class Condionals {
         System.out.print("Please Enter the Number to Find Day:");
         int DayNumber = Value.nextInt();
         switch (DayNumber) {
-            case 1:
+            case 1 ->
                 System.out.println("Monday");
-                break;
-            case 2:
+            case 2 ->
                 System.out.println("Tuseday");
-                break;
-            case 3:
+            case 3 ->
                 System.out.println("Wednesday");
-                break;
-            case 4:
+            case 4 ->
                 System.out.println("Thursday");
-                break;
-            case 5:
+            case 5 ->
                 System.out.println("Friday");
-                break;
-            case 6:
+            case 6 ->
                 System.out.println("Saturday");
-                break;
-            case 7:
+            case 7 ->
                 System.out.println("Sunday");
-                break;
         }
 
         // ? Fifth Set
