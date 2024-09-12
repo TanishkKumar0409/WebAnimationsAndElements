f30f7bfcba19afb7cad592c3de3155cea8f94e57
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-12 15:47:08 +0530

    update array.java

--- Array.java
+++ Array.java
@@ -5,47 +5,66 @@ public class Array {
         Scanner Value = new Scanner(System.in);
 
         // ! First Set
-        System.out.println("Enter the Values");
-        float[] Numbers = new float[5];
-        float Sum = 0F;
-        for (int i = 0; i <= Numbers.length - 1; i++) {
-            System.out.print(i + 1 + " Number: ");
-            Numbers[i] = Value.nextFloat();
-        }
+        // System.out.println("Enter the Values");
+        // float[] Numbers = new float[5];
+        // float Sum = 0F;
+        // for (int i = 0; i <= Numbers.length - 1; i++) {
+        // System.out.print(i + 1 + " Number: ");
+        // Numbers[i] = Value.nextFloat();
+        // }
 
-        for (int i = 0; i <= Numbers.length - 1; i++) {
-            Sum = Sum + Numbers[i];
-        }
-        System.out.println("The Sum of these Numbers: " + Sum);
+        // for (int i = 0; i <= Numbers.length - 1; i++) {
+        // Sum = Sum + Numbers[i];
+        // }
+        // System.out.println("The Sum of these Numbers: " + Sum);
 
         // ? Second Set
-        System.out.println("Enter the Values of Array:");
-        int[] SearchArray = new int[5];
-        for (int i = 0; i <= SearchArray.length - 1; i++) {
-            System.out.print(i + 1 + " Number: ");
-            SearchArray[i] = Value.nextInt();
-        }
+        // System.out.println("Enter the Values of Array:");
+        // int[] SearchArray = new int[5];
+        // for (int i = 0; i <= SearchArray.length - 1; i++) {
+        // System.out.print(i + 1 + " Number: ");
+        // SearchArray[i] = Value.nextInt();
+        // }
 
-        System.out.print("Enter the Value to Find: ");
-        int Search = Value.nextInt();
-        for (int i = 0; i <= SearchArray.length - 1; i++) {
-            if (SearchArray[i] == Search) {
-                System.out.println("This Number is Present in the array at index " + i +
-                        ".");
-            }
-        }
+        // System.out.print("Enter the Value to Find: ");
+        // int Search = Value.nextInt();
+        // for (int i = 0; i <= SearchArray.length - 1; i++) {
+        // if (SearchArray[i] == Search) {
+        // System.out.println("This Number is Present in the array at index " + i +
+        // ".");
+        // }
+        // }
 
         // * Third Set */
-        System.out.println("Enter the Marks:");
-        int[] Marks = new int[5];
-        int Total = 0;
-        for (int i = 0; i < Marks.length; i++) {
-            System.out.print(i + 1 + " Subject: ");
-            Marks[i] = Value.nextInt();
-            Total = Total + Marks[i];
+        // System.out.println("Enter the Marks:");
+        // int[] Marks = new int[5];
+        // int Total = 0;
+        // for (int i = 0; i < Marks.length; i++) {
+        // System.out.print(i + 1 + " Subject: ");
+        // Marks[i] = Value.nextInt();
+        // Total = Total + Marks[i];
+        // }
+        // float Average = Total / 5;
+        // System.out.println("Average Marks of Student: " + Average);
+
+        // ! Fourth Set
+        System.out.println("Enter The Array");
+        int[] RArray = new int[5];
+        int[] Reverse = new int[5];
+        // int Extra = 0;
+
+        for (int i = 0; i < RArray.length; i++) {
+            System.out.print(i + 1 + " Number: ");
+            RArray[i] = Value.nextInt();
         }
-        float Average = Total / 5;
-        System.out.println("Average Marks of Student: " + Average);
+        for (int i = 0; i < RArray.length; i++) {
+            Reverse[i] = RArray[RArray.length - i - 1];
+        }
+
+        System.out.println("Reverse Array");
+        for (int i = 0; i < Reverse.length; i++) {
+            System.out.println(i + 1 + " Number " + Reverse[i]);
+        }
 
         Value.close();
     }
