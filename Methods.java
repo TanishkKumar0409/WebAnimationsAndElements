3ecca1653d165520ed318d480655fc0eb58ab17b
TanishkKumar0409 <tanishkk60@gmail.com>
2024-10-02 10:36:08 +0530

    update

--- Methods.java
+++ Methods.java
@@ -7,11 +7,21 @@ public class Methods {
         }
     }
 
+    static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner Values = new Scanner(System.in);
         System.out.print("Enter The Number for Print table:");
         int Number = Values.nextInt();
         Table(Number);
+        pattern(Number);
         Values.close();
     }
 }
