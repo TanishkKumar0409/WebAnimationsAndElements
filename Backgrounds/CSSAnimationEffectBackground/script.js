b1f4746d367f25492d3b60a1f900b821e508db0d
TanishkKumar0409 <tanishkk60@gmail.com>
2024-11-07 16:21:28 +0530

    update

---
+++ Backgrounds/CSSAnimationEffectBackground/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let container = document.querySelector(".container");
+  for (let j = 0; j < 4; j++) {
+    let loader = document.createElement("div");
+    loader.classList.add("loader");
+    loader.style.setProperty("--j", j);
+    for (let i = 1; i <= 20; i++) {
+      let span = document.createElement("span");
+      span.style.setProperty("--i", i);
+      loader.appendChild(span);
+    }
+    container.appendChild(loader);
+  }
+  var angle = 0;
+  setInterval(() => {
+    container.style.filter = `hue-rotate(${angle}deg)`;
+    angle++;
+  }, 10);
+});
